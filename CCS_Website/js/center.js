e6baf8e2209ea181ae4b54bd0234ce4d8fb2e54a
Irtebat <irtebat.10@gmail.com>
2018-06-24 07:52:07 +0530

    About_progress_1

--- CCS_Website/js/center.js
+++ CCS_Website/js/center.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
     $('#abt_header').css('right',''+($(window).width() - $('#abt_header').width())/2+'px');
     $('#logo').css('right',''+($(window).width() - $('#logo').width())/2+'px');
     $('.intro').css('right',''+($(window).width() - $('.intro').width())/2+'px');
+    $('.Mainframe').css('right',''+($(window).width() - $('.Mainframe').width())/2+'px');
+    $('.FlipText').css({'right':''+($('.FlipBack').width() - $('.FlipText').width())/2+'px'});
 
 });
 
@@ -15,4 +17,6 @@ $(window).resize(function(){
     $('#abt_header').css('right',''+($(window).width() - $('#abt_header').width())/2+'px');
     $('#logo').css('right',''+($(window).width() - $('#logo').width())/2+'px');
     $('.intro').css('right',''+($(window).width() - $('.intro').width())/2+'px');
+    $('.Mainframe').css('right',''+($(window).width() - $('.Mainframe').width())/2+'px');
+    $('.FlipText').css('right',''+($('.FlipBack').width() - $('.FlipText').width())/2+'px');
 });
