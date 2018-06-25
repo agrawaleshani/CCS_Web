2b211409299502fe78bdf97821cd1007417cac03
Irtebat <irtebat.10@gmail.com>
2018-06-25 07:24:06 +0530

    Team section- partial code added

---
+++ CCS_Website/js/team.js
@@ -0,0 +1,23 @@
+$(document).scroll(function() {
+
+    scrollv=$(this).scrollTop();
+    var h=$(window).height()/1.8;
+    var condition=$('#tp2').offset().top-h;
+  
+    if(scrollv>condition)
+    {
+      $('figure').each(function(i) {
+  
+        setTimeout(function(){
+          $('figure').eq(i).addClass('showing');
+          },150*(i+1));
+      });
+    }
+  
+    if(scrollv<condition)
+    {
+      $('figure').removeClass('showing');
+    }
+  
+  });
+  
